fb7b88a1d818febe931cb92f46d1d9239b77faab
Bonbonbonsai <65070047@KMITL.AC.TH>
2022-07-25 16:37:14 +0700

    ssd to web

---
+++ airport.py
@@ -0,0 +1,10 @@
+"""airport"""
+def main();
+    """op"""
+    bang = str(input())
+    final = str(input())
+    bang_time = str(input())
+    if final == "Sydney (SYD)":
+        print("BKK - SYD")
+        print()
+main()
---
+++ machine.py
@@ -0,0 +1,11 @@
+"""machine"""
+def main():
+    """machine"""
+    num_1 = int(input())
+    value = int(input())
+    snack_1 = int(input())
+    snack_2 = int(input())
+    snack_3 = int(input())
+    total_1 = (num_1 - value) % 3
+    print(total_1)
+main()
